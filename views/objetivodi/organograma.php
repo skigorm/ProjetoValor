df3dcc1010582ae4e27cfefb655e3f8a3544d8b3
Igor Machado <igor.machado@invisual.com.br>
2018-12-07 13:27:06 -0200

    VLR-004: Inclusão do Organograma.

--- views/objetivodi/organograma.php
+++ views/objetivodi/organograma.php
@@ -10,11 +10,31 @@ use yii\grid\GridView;
 // $this->title = 'Diagrama';
 $this->params['breadcrumbs'][] = "$this->title";
 ?>
-<div class="objetivodi-index">
+
+<style>
+.objetivodi-index {
+  position: absolute;
+  top: 2;
+  left: 2;
+  right: 2;
+}
+
+iframe {
+  width: 100%;
+  height: 80vh;
+}
+</style>
+
+<div class="row">
+  <div class="col-md-12">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('../Organograma/index') ?>
+    <?php /*echo $this->render('../Organograma/index'); */ ?>
+
+    <iframe scrolling="auto" frameborder="0" src="http://localhost/Organograma/" "></iframe>
+    <br>
 
 
+</div>
 </div>
